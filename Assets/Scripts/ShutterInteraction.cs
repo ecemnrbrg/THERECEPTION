1364719f054c03c18123023360fb5b3c12809266
Ecem <ecemnurburgu52@gmail.com>
2026-05-15 23:57:19 +0300

    NEW

---
+++ Assets/Scripts/ShutterInteraction.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class ShutterInteraction : MonoBehaviour
+{
+    public Camera playerCamera;
+    public float interactDistance = 3f;
+    public float stopAfterSeconds = 1.2f;
+
+    public TextMeshProUGUI shutterText;
+
+    public Animator shutterAnimator;
+    public FPSController fpsController;
+
+    private bool shutterOpened = false;
+
+    void Start()
+    {
+        shutterText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (shutterOpened)
+            return;
+
+        CheckShutterButton();
+    }
+
+    void CheckShutterButton()
+    {
+        Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactDistance))
+        {
+            if (hit.collider.CompareTag("ShutterButton"))
+            {
+                shutterText.gameObject.SetActive(true);
+
+                if (!GameProgress.noteRead)
+                {
+                    shutterText.text = "Read the note first";
+                    return;
+                }
+
+                shutterText.text = "[E] Open Shutters";
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    OpenShutter();
+                }
+
+                return;
+            }
+        }
+
+        shutterText.gameObject.SetActive(false);
+    }
+
+    void OpenShutter()
+    {
+        shutterOpened = true;
+
+        shutterText.gameObject.SetActive(false);
+
+        shutterAnimator.SetTrigger("Open");
+
+        fpsController.canMove = true;
+
+        StartCoroutine(StopShutterAnimation());
+    }
+
+    IEnumerator StopShutterAnimation()
+    {
+        yield return new WaitForSeconds(stopAfterSeconds);
+
+        shutterAnimator.speed = 0f;
+    }
+}
